df73cc070fb016fbcc03959bb1e081bd47480124
Sokolov.Danila <Sokolov.Danila@sogaz.ru>
2026-03-05 12:48:24 +0300

    Добавлен headers exchange и routing_key

--- src/main/kotlin/ru/sogaz/site/orderingService/producer/OrderPaymentStatusEventProducerImpl.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/producer/OrderPaymentStatusEventProducerImpl.kt
@@ -3,6 +3,7 @@ package ru.sogaz.site.orderingService.producer
 import org.springframework.amqp.rabbit.connection.CorrelationData
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
+import ru.sogaz.site.loggingStarter.rabbitLogging.RabbitLogConst
 import ru.sogaz.site.orderingService.dto.data.CompletedPaymentData
 import ru.sogaz.site.orderingService.entity.OrderEntity
 import ru.sogaz.site.orderingService.mappers.order.PaidOrderMessagesMapper
@@ -25,6 +26,11 @@ class OrderPaymentStatusEventProducerImpl(
         rabbitProps.ordersExchange,
         requireNotNull(order.queueStatusResultName) { EMPTY_ROUTING_KEY_ERROR_MESSAGE },
         paidOrderMessagesMapper.toPaidOrderMessage(order, completedPaymentData),
+        { it.apply {
+                messageProperties.headers[RabbitLogConst.HDR_X_EXCHANGE] = rabbitProps.ordersExchange
+                messageProperties.headers[RabbitLogConst.HDR_X_ROUTINGKEY] = order.queueStatusResultName
+            }
+        },
         CorrelationData(order.orderId.toString()),
     )
 }
