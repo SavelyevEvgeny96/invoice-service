822b6ff59ab23998e53ae9e4eeccab9b444c337e
Sokolov.Danila <Sokolov.Danila@sogaz.ru>
2026-03-05 17:39:37 +0300

    Фикс __TypeId__

--- src/main/kotlin/ru/sogaz/site/orderingService/producer/OrderPaymentStatusEventProducerImpl.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/producer/OrderPaymentStatusEventProducerImpl.kt
@@ -5,16 +5,18 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
 import ru.sogaz.site.loggingStarter.rabbitLogging.RabbitLogConst
 import ru.sogaz.site.orderingService.dto.data.CompletedPaymentData
+import ru.sogaz.site.orderingService.dto.data.PaidOrderMessage
 import ru.sogaz.site.orderingService.entity.OrderEntity
 import ru.sogaz.site.orderingService.mappers.order.PaidOrderMessagesMapper
 import ru.sogaz.site.orderingService.properties.RabbitProps
 
 @Component
 class OrderPaymentStatusEventProducerImpl(
-    private val rabbitTemplate: RabbitTemplate,
+    rabbitTemplate: RabbitTemplate,
     private val rabbitProps: RabbitProps,
     private val paidOrderMessagesMapper: PaidOrderMessagesMapper,
-) : OrderPaymentStatusEventProducer {
+) : RabbitProducer<PaidOrderMessage>(rabbitTemplate),
+    OrderPaymentStatusEventProducer {
     companion object {
         private const val EMPTY_ROUTING_KEY_ERROR_MESSAGE = "Для заказа не указана целевая очередь для отправки статуса оплаты"
     }
@@ -22,16 +24,10 @@ class OrderPaymentStatusEventProducerImpl(
     override fun sendPaymentOrderEvent(
         order: OrderEntity,
         completedPaymentData: CompletedPaymentData,
-    ) = rabbitTemplate.convertAndSend(
+    ) = convertAndSend(
         rabbitProps.ordersExchange,
         requireNotNull(order.queueStatusResultName) { EMPTY_ROUTING_KEY_ERROR_MESSAGE },
         paidOrderMessagesMapper.toPaidOrderMessage(order, completedPaymentData),
-        {
-            it.apply {
-                messageProperties.headers[RabbitLogConst.HDR_X_EXCHANGE] = rabbitProps.ordersExchange
-                messageProperties.headers[RabbitLogConst.HDR_X_ROUTINGKEY] = order.queueStatusResultName
-            }
-        },
-        CorrelationData(order.orderId.toString()),
+        order.orderId,
     )
 }
