2423c669971614a440d2c56497e0b076918b5455
Sokolov.Danila <Sokolov.Danila@sogaz.ru>
2026-03-24 15:40:43 +0300

    [SITE-2496] Правка зависимости от mainContractCheck при формировании описания на возврат
    (cherry picked from commit 5b9d0145c39d94406307f43b63c1a0282bdee7a1)

--- src/main/kotlin/ru/sogaz/site/orderingService/dao/SubOrderDao.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/dao/SubOrderDao.kt
@@ -6,7 +6,7 @@ import java.util.UUID
 interface SubOrderDao {
     fun upsertSubOrders(subs: List<SubOrderEntity>)
 
-    fun findByOrderIdAndMainContractCheck(orderId: UUID?): SubOrderEntity?
+    fun findFirstByOrderEntityOrderId(orderId: UUID?): SubOrderEntity?
 
     fun findByOrderId(orderId: UUID?): List<SubOrderEntity?>
 }
