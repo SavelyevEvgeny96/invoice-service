a94d99ed9ff26392d5333eb9f9d60c7bfdbcaf1a
Соколов Данила Андреевич <sokolov.danila@sogaz.ru>
2026-04-02 07:38:05 +0000

    fix SentReceiptData.kt

--- src/main/kotlin/ru/sogaz/site/orderingService/dto/data/SentReceiptData.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/dto/data/SentReceiptData.kt
@@ -4,12 +4,12 @@ import java.time.Instant
 import java.util.UUID
 
 class SentReceiptData(
-    val receiptId: UUID,
-    val orderId: UUID?,
-    val paymentId: UUID,
-    val amount: String,
+    val receiptId: UUID?,
+    val orderId: UUID,
+    val paymentId: UUID?,
+    val amount: String?,
     val state: String,
-    val typeOperation: String,
+    val typeOperation: String?,
     val sendingTime: Instant?,
     val link: String?,
     val errorText: String?,
