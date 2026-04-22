d62a4df4280d1530e41c0bebb840fddc23a87a73
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-04-22 14:15:07 +0300

    Фикс

--- src/main/kotlin/ru/sogaz/site/orderingService/config/ShortLinksClientConfig.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/config/ShortLinksClientConfig.kt
@@ -3,11 +3,19 @@ package ru.sogaz.site.orderingService.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-
+import ru.sogaz.site.shortlinks.client.api.ShortLinkControllerApi
+import ru.sogaz.site.shortlinks.client.invoker.ApiClient
 
 @Configuration
 class ShortLinksClientConfig {
+
     @Bean
-    fun shortLinkControllerApi(@Value("\${app.client.shortLink.basePath}") shortLinkBasePath: String) =
-        ru.sogaz.site.shortlinks.client.invoker.ApiClient().apply { basePath = shortLinkBasePath }
+    fun shortLinkControllerApi(
+        @Value("\${app.client.shortLink.basePath}") shortLinkBasePath: String
+    ): ShortLinkControllerApi {
+        val apiClient = ApiClient().apply {
+            basePath = shortLinkBasePath
+        }
+        return ShortLinkControllerApi(apiClient)
+    }
 }
