36b0051c00f0ee9e79ab4291c80a72ffe890129f
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-04-28 12:07:44 +0300

    SITE-3008 add system ordering

--- src/main/resources/db.changelog/v1.0.8/2026-04-28/DATA/add_system.sql
+++ src/main/resources/db.changelog/v1.0.8/2026-04-28/DATA/add_system.sql
@@ -1,3 +1,13 @@
-ALTER TABLE payment_operations
-    ADD COLUMN IF NOT EXISTS payment_system VARCHAR(255),
-    ADD COLUMN IF NOT EXISTS external_error_code VARCHAR(255);
+INSERT INTO client_systems (
+    external_system_code,
+    external_system_name,
+    permission_return,
+    skip_sending_errors_queue
+)
+VALUES
+    ('ordering-service-client', 'Сервис заказов', false, false)
+ON CONFLICT (external_system_code) DO UPDATE
+SET external_system_name = EXCLUDED.external_system_name,
+    permission_return = EXCLUDED.permission_return,
+    skip_sending_errors_queue = EXCLUDED.skip_sending_errors_queue,
+    update_date = now();
