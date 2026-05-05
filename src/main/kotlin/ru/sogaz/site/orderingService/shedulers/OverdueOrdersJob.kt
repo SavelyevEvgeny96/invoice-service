85e6f730ea2316f65265748b725ee4e99f36d391
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-05-05 12:11:50 +0300

    ЛОГИ джоба

--- src/main/kotlin/ru/sogaz/site/orderingService/shedulers/OverdueOrdersJob.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/shedulers/OverdueOrdersJob.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import ru.sogaz.site.orderingService.dto.data.OverdueOrdersEvent
 import ru.sogaz.site.orderingService.enums.OrderStatusesEnum
+import ru.sogaz.site.orderingService.loggerFor
 import ru.sogaz.site.orderingService.repository.OrderRepository
 import java.time.Clock
 import java.time.Instant
@@ -17,12 +18,14 @@ class OverdueOrdersJob(
     private val eventPublisher: ApplicationEventPublisher,
     private val clock: Clock,
 ) {
+    private val logger = loggerFor(javaClass)
+
     @Scheduled(cron = "\${crons.overdue.cron}")
     @SchedulerLock(name = "overdueOrdersJob")
     @Transactional
     fun run() {
         val now: Instant = Instant.now(clock)
-
+        logger.info("Запуск job -> удаление просроченных order")
         val orders =
             orderRepository.findOverdueOrders(
                 states = listOf(OrderStatusesEnum.NEW, OrderStatusesEnum.UPDATE),
@@ -34,6 +37,7 @@ class OverdueOrdersJob(
         orders.forEach { order ->
             order.status = OrderStatusesEnum.OVERDUE
             order.updateDate = now
+            logger.info("Обновлен статус у orderId:${order.orderId}")
         }
 
         eventPublisher.publishEvent(OverdueOrdersEvent(orders))
