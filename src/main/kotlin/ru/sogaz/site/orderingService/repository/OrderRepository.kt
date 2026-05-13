bb5ce809d6ac193a6547b46fb0197feafc6f4687
Evgeny Savelev <Savelev.Evgeny.V@sogaz.ru>
2026-05-13 11:58:40 +0300

    Refactor

--- src/main/kotlin/ru/sogaz/site/orderingService/repository/OrderRepository.kt
+++ src/main/kotlin/ru/sogaz/site/orderingService/repository/OrderRepository.kt
@@ -1,6 +1,8 @@
 package ru.sogaz.site.orderingService.repository
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
@@ -12,6 +14,7 @@ import java.util.UUID
 
 @Repository
 interface OrderRepository : JpaRepository<OrderEntity, UUID> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     override fun findById(id: UUID): Optional<OrderEntity>
 
     fun findAllByRecipientUserId(userId: String): List<OrderEntity?>
