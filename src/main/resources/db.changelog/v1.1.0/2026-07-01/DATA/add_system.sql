1f3ea7d39137c2d9be2d2d4fb83fe8925401632d
Savelev Evgeny <Savelev.Evgeny.V@sogaz.ru>
2026-07-01 00:20:39 +0300

    storefront-client ADD

---
+++ src/main/resources/db.changelog/v1.1.0/2026-07-01/DATA/add_system.sql
@@ -0,0 +1,13 @@
+INSERT INTO client_systems (
+    external_system_code,
+    external_system_name,
+    permission_return,
+    skip_sending_errors_queue
+)
+VALUES
+    ('storefront-client', 'МП', false, false)
+ON CONFLICT (external_system_code) DO UPDATE
+SET external_system_name = EXCLUDED.external_system_name,
+    permission_return = EXCLUDED.permission_return,
+    skip_sending_errors_queue = EXCLUDED.skip_sending_errors_queue,
+    update_date = now();
